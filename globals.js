f690884739f6c9c1b2f85cd8ac1972fc80dac4c4
HansCris01 <core512l@outlook.com>
2025-05-14 18:28:21 -0500

    subiendo cambios

---
+++ globals.js
@@ -0,0 +1,10 @@
+
+console.log(__dirname); //nos va dar la ubicación del directorio
+
+console.log(__filename); //nos va dar la ubicación del archivo
+
+console.log(module); //te permite ver los módulos
+
+console.log(require); //te muestra las importaciones de módulo
+
+console.log(process); //te da información del sistema
---
+++ index.js
@@ -0,0 +1,19 @@
+//console.log("Hello World");
+
+let username = "dev512";
+let age = 30;
+let hasHobbies=true;
+let points = [10,20,30];
+let user = {
+    name: "Hans",
+    lastname: "Luyo"
+};
+
+const PI = 3.14;
+
+console.log(username);
+console.log(age);
+console.log(hasHobbies);
+console.log(points);
+console.log(user);
+console.log(PI);
